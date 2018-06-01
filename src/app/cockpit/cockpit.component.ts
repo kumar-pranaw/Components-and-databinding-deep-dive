1a28887bb66bbc4494011bc596b74b45f7b1966b
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-06-01 17:45:23 +0530

    Directives and decorators

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAddServer() {
+  this.serverCreated.emit({
+    serverName: this.newServerName,
+    serverContent: this.newServerContent
+  });
+  }
+
+  onAddBlueprint() {
+this.bluePrintCreated.emit({
+  serverName: this.newServerName,
+  serverContent: this.newServerContent
+});
+  }
+
+}
